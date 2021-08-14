610f6d12696b3331ee22b87033d503cc264b1366
Gunzelb <brandongunzel@gmail.com>
2021-08-14 13:40:11 -0700

    init commit

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    env: {
+        node: true,
+        es2021: true,
+    },
+    extends: ['airbnb-base', 'prettier'],
+    parserOptions: {
+        ecmaVersion: 12,
+    },
+    rules: {},
+}
