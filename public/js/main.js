8cb86b419be8f03e670af88fc625dd7b760155dd
Brian Moreno <BMore-Creative@users.noreply.github.com>
2021-08-23 08:56:58 -0700

    added styling: navbar/footer styling complete, including addition of logo/icon. added elements: handlebars homepage set up, troubleshooting with mock data to be done

--- public/js/main.js
+++ public/js/main.js
@@ -1,42 +1,60 @@
 //DOM element variables
+const menuNavEl = document.querySelector('#navbarMenu');
+const burgerNavEl = document.querySelector('#navbarBurger');
 const homeNavEl = document.querySelector('#homeA');
 const loginNavEl = document.querySelector('#login');
 const userNavEl = document.querySelector('#userProjects');
 const createNavEl = document.querySelector('#newProject');
 const projListEl = document.querySelector('#projectList')
 
+let toggleMenu = (event) => {
+    event.preventDefault();
+    menuNavEl.classList.toggle('is-active');
+};
+
 //Login State Boolean
 let loggedIn = false;
 
 let isLoggedIn = () => {
     if (loggedIn === true) {
-        loginNavEl.setAttribute('class', 'hide');
+        loginNavEl.classList.toggle('hide');
 
-        userNavEl.removeAttribute('class');
-        userNavEl.setAttribute('class','');
+        userNavEl.classList.toggle('hide');
 
-        createNavEl.removeAttribute('class');
-        createNavEl.setAttribute('class','');
+        createNavEl.classList.toggle('hide');
     }
 };
+
+
 
 let displayProjects = () => {
     const allProjects = 'database route here'
 
     allProjects.forEach(function (project) {
-        const project = document.createElement('div');
+        const projectEl = document.createElement('div');
         const h3 = document.createElement('h3');
         const pDetails = document.createElement('p');
         const pCurrFund = document.createElement('p');
         const pGoalFund = document.createElement('p');
 
-        project.setAttribute('class', '');
-        projListEl.appendChild(project);
+        projectEl.setAttribute('class', '');
+        projListEl.appendChild(projectEl);
 
-        
+        h3.textContent = project.name;
+        projectEl.appendChild(h3);
 
+        pDetails.textContent = project.details;
+        projectEl.appendChild(pDetails);
+
+        const currFund = project.funded;
+
+        pCurrFund.textContent = `$${currFund} out of `
+        projectEl.appendChild(pCurrFund);
+
+        pGoalFund.textContent = project.goal;
+        pCurrFund.appendChild(pGoalFund);
     });
-
-
-
 }
+
+burgerNavEl.onclick = toggleMenu;
+isLoggedIn();
