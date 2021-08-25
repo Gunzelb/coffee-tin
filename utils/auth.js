798c8d86035971d41f9dce406f0c37e6cccaa297
Brian Moreno <BMore-Creative@users.noreply.github.com>
2021-08-24 19:53:45 -0700

    pre-merge

--- utils/auth.js
+++ utils/auth.js
@@ -1,22 +1,12 @@
 const { User } = require('../models')
 
 const authenticate = async (req, res, next) => {
-    if (req.session.logged_in) {
-        const user = await User.findByPk(req.session.user_id, {
-            attributes: {
-                exclude: ['password'],
-            },
-        })
-
-        req.user = user.get({ plain: true })
+    // If the user is not logged in, redirect the request to the login route
+    if (!req.session.logged_in) {
+        res.redirect('/login')
 
         return next()
     }
-
-    // If the user is not logged in, redirect the request to the login route
-    res.redirect('/login')
-
-    return next()
 }
 
 module.exports = authenticate
