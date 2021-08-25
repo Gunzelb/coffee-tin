ab82f18cbfcb7b0fd4c61586eac72b1e1444a2ef
Brian Moreno <BMore-Creative@users.noreply.github.com>
2021-08-24 19:38:00 -0700

    added styling: project reward tiers now render to page correctly. Login/Register Page styled correctly. added functionality: login form captures and sends info to backend, basic auth function created, troubleshooting required.

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,6 @@
 //DOM element variables
 const menuNavEl = document.querySelector('#navbarMenu');
 const burgerNavEl = document.querySelector('#navbarBurger');
-const homeNavEl = document.querySelector('#homeA');
 const loginNavEl = document.querySelector('#login');
 const userNavEl = document.querySelector('#userProjects');
 const createNavEl = document.querySelector('#newProject');
@@ -24,37 +23,6 @@ let isLoggedIn = () => {
         createNavEl.classList.toggle('hide');
     }
 };
-
-
-
-let displayProjects = () => {
-    const allProjects = 'database route here'
-
-    allProjects.forEach(function (project) {
-        const projectEl = document.createElement('div');
-        const h3 = document.createElement('h3');
-        const pDetails = document.createElement('p');
-        const pCurrFund = document.createElement('p');
-        const pGoalFund = document.createElement('p');
-
-        projectEl.setAttribute('class', '');
-        projListEl.appendChild(projectEl);
-
-        h3.textContent = project.name;
-        projectEl.appendChild(h3);
-
-        pDetails.textContent = project.details;
-        projectEl.appendChild(pDetails);
-
-        const currFund = project.funded;
-
-        pCurrFund.textContent = `$${currFund} out of `
-        projectEl.appendChild(pCurrFund);
-
-        pGoalFund.textContent = project.goal;
-        pCurrFund.appendChild(pGoalFund);
-    });
-}
 
 burgerNavEl.onclick = toggleMenu;
 isLoggedIn();
